88bdfb11c916935f815ecdbebb844280c3dab570
redshiftzero <jen@redshiftzero.com>
2017-05-16 17:33:37 -0700

    Add unit tests for new view functions

--- securedrop/directory/tests/test_views.py
+++ securedrop/directory/tests/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils.html import escape
 
 from directory.forms import LandingPageForm
-from directory.views import home_page
+from directory.views import landing_page_test
 
 
 class HomePageTest(TestCase):
@@ -10,16 +10,26 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_includes_landing_page_form(self):
-        response = self.client.get('/')
+
+class SecuredropDirectoryTest(TestCase):
+    def test_securedrop_directory_renders_home_template(self):
+        response = self.client.get('/directory', follow=True)
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class LandingPageScannerTest(TestCase):
+    def landing_page_scanner_renders_correct_template(self):
+        response = self.client.get('/test')
+        self.assertTemplateUsed(response, 'landing_page_test.html')
+
+    def test_landing_page_scanner_includes_landing_page_form(self):
+        response = self.client.get('/test', follow=True)
         self.assertIsInstance(response.context['form'], LandingPageForm)
 
-    def post_invalid_input(self):
-        return self.client.post(
-            '/',
-            data = {'url': ''}
+    def test_invalid_input_returns_to_landing_page_scanner(self):
+        response = self.client.post(
+            '/test',
+            data = {'url': ''},
+            follow=True
         )
-
-    def test_invalid_input_returns_home(self):
-        response = self.post_invalid_input()
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'landing_page_test.html')
