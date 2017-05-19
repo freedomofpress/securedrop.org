5b9cc30d28142a716a32a77c695ada119db889ed
redshiftzero <jen@redshiftzero.com>
2017-05-19 00:27:11 -0700

    Add results to admin interface

--- securedrop/directory/admin.py
+++ securedrop/directory/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from directory.models import Securedrop
+from directory.models import Securedrop, Result
 
 
 admin.site.site_header = 'SecureDrop'
 admin.site.index_title = ''
 
 admin.site.register(Securedrop)
+admin.site.register(Result)
