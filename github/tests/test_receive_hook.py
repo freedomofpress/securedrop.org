2ebdfed226779fd0240449ba2876a5e841908e6f
Emily Horsman <me@emilyhorsman.com>
2017-08-08 17:10:21 -0700

    Add tests for release events from the GitHub hook

---
+++ github/tests/test_receive_hook.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+import os
+
+from django.http.request import HttpRequest
+from django.test import TestCase, Client
+from django.urls import reverse
+import hashlib
+import hmac
+
+from github.models import Release
+
+
+class TestReceiveHook(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_ignore_non_release_events(self):
+        json_path = os.path.join(
+            os.path.dirname(__file__),
+            'success_receive_hook.json',
+        )
+        with open(json_path, 'r') as handler:
+            payload = handler.read()
+            mac = hmac.new(b'test', msg=payload.encode('utf-8'), digestmod=hashlib.sha1)
+
+        response = self.client.post(
+            reverse('github:receive-hook'),
+            data=payload,
+            content_type='application/json',
+            HTTP_X_GITHUB_EVENT='commit',
+            HTTP_X_HUB_SIGNATURE='sha1={}'.format(mac.hexdigest()),
+        )
+        self.assertEqual(Release.objects.count(), 0)
+
+    def test_incorrect_signature(self):
+        json_path = os.path.join(
+            os.path.dirname(__file__),
+            'success_receive_hook.json',
+        )
+        with open(json_path, 'r') as handler:
+            payload = handler.read()
+            mac = hmac.new(b'test', msg=(payload + 'invalid').encode('utf-8'), digestmod=hashlib.sha1)
+
+        response = self.client.post(
+            reverse('github:receive-hook'),
+            data=payload,
+            content_type='application/json',
+            HTTP_X_GITHUB_EVENT='release',
+            HTTP_X_HUB_SIGNATURE='sha1={}'.format(mac.hexdigest()),
+        )
+        self.assertEqual(Release.objects.count(), 0)
+
+    def test_successful_release(self):
+        json_path = os.path.join(
+            os.path.dirname(__file__),
+            'success_receive_hook.json',
+        )
+        with open(json_path, 'r') as handler:
+            payload = handler.read()
+            mac = hmac.new(b'test', msg=payload.encode('utf-8'), digestmod=hashlib.sha1)
+
+        response = self.client.post(
+            reverse('github:receive-hook'),
+            data=payload,
+            content_type='application/json',
+            HTTP_X_GITHUB_EVENT='release',
+            HTTP_X_HUB_SIGNATURE='sha1={}'.format(mac.hexdigest()),
+        )
+        release = Release.objects.last()
+        self.assertEqual(release.tag_name, 'v0.1.0')
+        self.assertEqual(
+            release.date,
+            datetime(2017, 8, 8, 21, 38, 21, tzinfo=timezone.utc)
+        )
