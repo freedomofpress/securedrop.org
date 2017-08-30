ae109a78bf3a7ab04d44b7f98e69a3428a0234c2
racheltstevens <racheltstevens@gmail.com>
2017-08-30 11:58:00 -0400

    Add homepage description
    Adds a description title to be the subheader for the page.

--- home/models.py
+++ home/models.py
@@ -1,6 +1,29 @@
+from django.db import models
+
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
+
 from common.models import MetadataPageMixin
 
 
 class HomePage(MetadataPageMixin, Page):
-    pass
+    description_header = models.CharField(max_length=255, blank=True, null=True)
+    # Disables headers and image/video embeds
+    description = RichTextField(
+        features=['bold', 'italic', 'ol', 'ul', 'hr', 'link', 'document-link' ],
+        blank=True,
+        null=True
+    )
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel('description_header'),
+                FieldPanel('description'),
+            ],
+            "Description"
+        )
+    ]
+
+
