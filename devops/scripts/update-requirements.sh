9fdc2a19a0a44c36803179abae4e2cbad372b3ba
Michael Sheinberg <mike@freedom.press>
2017-09-07 11:29:52 -0400

    Enforce update reqs script to utilize python3
    Noticed a large failure when running pip-compile under a python2 environment and
    then having the python3 dev docker try to install those dependencies. This
    change enforces local dev using python3 virtualenv instead.

--- devops/scripts/update-requirements.sh
+++ devops/scripts/update-requirements.sh
@@ -2,8 +2,12 @@
 #
 # Compile requirements files
 
-if [ ! -f devops/.venv/bin/activate ]; then virtualenv --no-site-packages devops/.venv; fi
-source devops/.venv/bin/activate
+if [ ! -f devops/.compile-venv/bin/activate ]; then
+    virtualenv -p "$(which python3)" --no-site-packages devops/.compile-venv
+fi
+
+source devops/.compile-venv/bin/activate
+pip install pip-tools
 
 pip-compile --output-file requirements.txt requirements.in
 pip-compile --output-file dev-requirements.txt dev-requirements.in
