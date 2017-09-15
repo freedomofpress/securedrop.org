f192a2fe400a1432767464c5cf1c24bb13fd7a04
Cameron Higby-Naquin <me@chigby.org>
2017-09-15 14:42:45 -0400

    Clean up way of generating fake onion addresses

--- landing_page_checker/tests/factories.py
+++ landing_page_checker/tests/factories.py
@@ -8,6 +8,13 @@ from directory.tests.factories import DirectoryPageFactory
 from landing_page_checker.models import Securedrop
 
 
+def random_onion_address():
+    return ''.join(
+        random.choice(string.ascii_lowercase + string.digits)
+        for _ in range(16)
+    ) + '.onion'
+
+
 class SecuredropFactory(factory.Factory):
     class Meta:
         model = Securedrop
@@ -16,9 +23,4 @@ class SecuredropFactory(factory.Factory):
     organization = factory.Faker('sentence', nb_words=3)
     slug = factory.LazyAttribute(lambda o: slugify(o.organization))
     landing_page_domain = factory.Faker('uri')
-    onion_address = (
-        ''.join(
-            random.choice(string.ascii_lowercase + string.digits)
-            for _ in range(16)
-        )
-    )
+    onion_address = factory.LazyFunction(random_onion_address)
