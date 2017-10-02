5e035363091242853e237ba516c5375b477443fb
racheltstevens <racheltstevens@gmail.com>
2017-10-02 17:17:11 -0400

    Add fields to directory model
    So that we have all the info we want to display.

--- directory/models.py
+++ directory/models.py
@@ -1,18 +1,46 @@
+from django.db import models
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin, route
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailadmin.edit_handlers import (
     InlinePanel,
 )
 
+from common.models.mixins import MetadataPageMixin
 from directory.forms import DirectoryForm
 from landing_page_checker.landing_page import scanner
 from landing_page_checker.models import SecuredropPage as SecuredropInstance
 
 
-class DirectoryPage(RoutablePageMixin, Page):
+class DirectoryPage(RoutablePageMixin, MetadataPageMixin, Page):
+    body = RichTextField(blank=True, null=True)
+    source_warning = RichTextField(blank=True, null=True, help_text="A warning for sources about checking onion addresses.")
+    submit_title = models.CharField(max_length=255, default="Want to get your instance listed?")
+    submit_body = RichTextField(blank=True, null=True)
+    submit_button_text = models.CharField(
+        max_length=100,
+        default="Scan my instance",
+        help_text="Text displayed on link to scanning form.")
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        MultiFieldPanel(
+            heading="Sidebar",
+            children=[
+                FieldPanel('source_warning'),
+                FieldPanel('submit_title'),
+                FieldPanel('submit_body'),
+                FieldPanel('submit_button_text')
+            ],
+            classname='collapsible'
+        )
+    ]
+
+
     @route('form/')
     def form_view(self, request):
         if request.method == 'POST':
