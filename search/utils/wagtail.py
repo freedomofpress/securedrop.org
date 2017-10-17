49725e0f9f75678e1c7ad9056e5dc0dd488cca93
Harris Lapiroff <harris@chromamine.com>
2017-10-17 13:59:18 -0400

    Restructure management command
    - Abstract out utilities to update wagtail page SearchDocuments
      singly or in a queryset
    - Rename management command from `rebuild_wagtail_index` to
      `update_wagtail_index` and add a `--rebuild` flag to flush
      the wagtail page SearchDocuments first

---
+++ search/utils/wagtail.py
@@ -0,0 +1,54 @@
+from search.models import SearchDocument
+
+
+def index_wagtail_page(page):
+    """
+    Create or update a search document for a Wagtail Page. Returns an
+    (object, created) tuple, where object is the created or updated object and
+    created is a boolean specifying whether a new object was created.
+
+    """
+
+    # Make sure we have the most specific page subclass instance
+    page = page.specific
+
+    # We can't index the root page
+    if page.is_root():
+        return
+
+    # Get search content from page instance
+    search_content = page.get_search_content() if hasattr(page, 'get_search_content') else ''
+
+    # Create a new SearchDocument
+    document_key = 'wagtail-page-{}'.format(page.pk)
+    result = SearchDocument.objects.update_or_create(
+        {
+            'title': page.title,
+            'url': page.full_url,
+            'search_content': search_content,
+            'data': {},
+            'result_type': 'W',
+            'key': document_key,
+        },
+        key=document_key
+    )
+
+    return result
+
+
+def index_wagtail_pages(queryset):
+    """
+    Create or update search documents for multiple Wagtail Pages. Returns a list
+    of (object, created) tuples, where object is the created or updated object
+    and created is a boolean specifying whether a new object was created.
+
+    """
+
+    # Make sure we get only live pages in their most specific subclass instance
+    pages = filter(lambda x: not x.is_root(), queryset.live().specific())
+
+    results = []
+    for page in pages:
+        results.append(index_wagtail_page(page))
+
+    return results
