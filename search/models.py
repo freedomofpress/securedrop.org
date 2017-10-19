43c2c6b5ee425e8ca93b4d511b52532a07de7ce1
Harris Lapiroff <harris@chromamine.com>
2017-10-19 14:01:52 -0400

    Feed flake8

--- search/models.py
+++ search/models.py
@@ -1,13 +1,14 @@
 from django.contrib.postgres.fields import HStoreField
 from django.db import models
 
+
 class SearchDocument(models.Model):
     RESULT_TYPES = (
         ('F', 'Forum'),
         ('D', 'Documentation'),
         ('W', 'Wagtail Page')
     )
-    
+
     title = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
     search_content = models.TextField()
--- search/tests/factories.py
+++ search/tests/factories.py
@@ -2,6 +2,7 @@ import factory
 
 from search.models import SearchDocument
 
+
 class SearchDocumentFactory(factory.django.DjangoModelFactory):
     title = factory.Sequence(lambda n: 'Document {}'.format(n))
     data = {}
