40c4a583200564ce9c4ae7efb18ad0c2c5b28ba9
Harris Lapiroff <harris@chromamine.com>
2017-10-19 13:57:15 -0400

    Add tests for the search view

---
+++ search/tests/factories.py
@@ -0,0 +1,10 @@
+import factory
+
+from search.models import SearchDocument
+
+class SearchDocumentFactory(factory.django.DjangoModelFactory):
+    title = factory.Sequence(lambda n: 'Document {}'.format(n))
+    data = {}
+
+    class Meta:
+        model = SearchDocument
