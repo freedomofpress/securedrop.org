98bc413f6785e05c0f2fbffca9b759a8f75c1015
racheltstevens <racheltstevens@gmail.com>
2017-10-20 11:57:42 -0400

    Make securedrop lists/details only available to owners

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
 from landing_page_checker.models import SecuredropPage
 from django.views.generic import ListView, DetailView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 
+
+@method_decorator(login_required, name='dispatch')
 class SecuredropList(ListView):
     model = SecuredropPage
 
+    def get_queryset(self, **kwargs):
+        queryset = super(SecuredropList, self).get_queryset(**kwargs)
+        print(self.request.user)
+        return queryset.filter(owners__owner=self.request.user)
+
+
+@method_decorator(login_required, name='dispatch')
 class SecuredropDetail(DetailView):
     model = SecuredropPage
+
+    def get_object(self, **kwargs):
+        obj = super(SecuredropDetail, self).get_object(**kwargs)
+        if self.request.user not in [ o.owner for o in obj.owners.all() ]:
+            raise PermissionDenied
+        return obj
