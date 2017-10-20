3d67be3f2104e90f69db846ff5c71d04c3bc77c7
racheltstevens <racheltstevens@gmail.com>
2017-10-20 15:35:41 -0400

    Add autocomplete sass for non-wagtail widgets
    Change variables to variables in this project

--- directory/forms.py
+++ directory/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from captcha.fields import ReCaptchaField
 
+from wagtail.wagtailimages.widgets import AdminImageChooser
+from autocomplete.widgets import Autocomplete
+from directory.models import Language
+from common.models import CustomImage
 
 class DirectoryForm(forms.Form):
     organization = forms.CharField(label="Organization", max_length=255)
     url = forms.URLField()
     tor_address = forms.CharField(label="Tor address", max_length=255)
+    language = forms.ModelMultipleChoiceField(queryset=Language.objects.all(), widget=type(
+                '_Autocomplete',
+                (Autocomplete,),
+                dict(page_type=Language, can_create=True, is_single=True)
+            ))
 
 
 class ScannerForm(forms.Form):
