c7182b1bd6c042774fc0ea0de4ecf2bdff1c079d
racheltstevens <racheltstevens@gmail.com>
2017-10-23 14:55:52 -0400

    Display autocomplete correctly

--- directory/forms.py
+++ directory/forms.py
@@ -11,7 +11,7 @@ class DirectoryForm(forms.Form):
     organization = forms.CharField(label="Organization", max_length=255)
     url = forms.URLField()
     tor_address = forms.CharField(label="Tor address", max_length=255)
-    language = forms.ModelMultipleChoiceField(queryset=Language.objects.all(), widget=type(
+    languages_accepted = forms.ModelMultipleChoiceField(queryset=Language.objects.all(), widget=type(
                 '_Autocomplete',
                 (Autocomplete,),
                 dict(page_type='directory.Language', can_create=True, is_single=False, api_base='/autocomplete/')
