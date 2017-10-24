e5a2eb10c064224ee8ced87da7ef6070fbf606a4
Harris Lapiroff <harris@chromamine.com>
2017-10-24 17:02:38 -0400

    Ensure SearchDocumentFactory creates documents with unique keys

--- search/tests/factories.py
+++ search/tests/factories.py
@@ -6,6 +6,7 @@ from search.models import SearchDocument
 class SearchDocumentFactory(factory.django.DjangoModelFactory):
     title = factory.Sequence(lambda n: 'Document {}'.format(n))
     data = {}
+    key = factory.Sequence(lambda n: 'Key {}'.format(n))
 
     class Meta:
         model = SearchDocument
