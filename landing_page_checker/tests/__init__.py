37b3379eb27bc5578c2049a7d773491594d72d86
racheltstevens <racheltstevens@gmail.com>
2017-10-26 18:10:02 -0400

    Add search tests for SecureDrop page

--- landing_page_checker/tests/__init__.py
+++ landing_page_checker/tests/__init__.py
@@ -1,2 +1,3 @@
 from landing_page_checker.tests.test_forms import *  # noqa: F403,F401
 from landing_page_checker.tests.test_models import *  # noqa: F403,F401
+from landing_page_checker.tests.test_search_content import *  # noqa: F403,F401
