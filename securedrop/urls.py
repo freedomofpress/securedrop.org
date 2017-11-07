ba8cc72e919b608e791b1127714facc6d45d9eb5
racheltstevens <racheltstevens@gmail.com>
2017-11-07 15:39:31 -0500

    Add urls for error pages
    Close #290
    I had a misunderstanding of how Django renders error pages, and we need these routes, so here they are.

--- securedrop/urls.py
+++ securedrop/urls.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 from search import views as search_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
@@ -28,6 +29,9 @@ urlpatterns = [
     url(r'', include(account_urls)),
 
     url(r'', include(wagtail_urls)),
+    url(r'^500/$', TemplateView.as_view(template_name="500.html")),
+    url(r'^404/$', TemplateView.as_view(template_name="404.html")),
+    url(r'^403/$', TemplateView.as_view(template_name="403.html")),
 ]
 
 
