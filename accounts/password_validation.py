7eb20acbbf31f6de424fa54cdb9fa1e2d26afdee
Stephen Burrows <stephen.r.burrows@gmail.com>
2017-11-16 11:51:39 -0800

    Added zxcvbn password validation server-side

---
+++ accounts/password_validation.py
@@ -0,0 +1,20 @@
+from django.core.exceptions import ValidationError
+from zxcvbn import zxcvbn
+
+
+class ZxcvbnValidator(object):
+    def validate(self, password, user=None):
+        user_inputs = []
+        if user:
+            user_inputs = [
+                user.email,
+                user.username,
+                user.first_name,
+                user.last_name,
+            ]
+        result = zxcvbn(password, user_inputs=user_inputs)
+        if result['score'] < 4:
+            warning = result['feedback']['warning']
+            suggestions = result['feedback']['suggestions']
+            messages = [warning] + suggestions if warning else suggestions
+            raise ValidationError(messages)
