76f1b9eccd009652308bbd55ee121d02e36af5f3
racheltstevens <racheltstevens@gmail.com>
2017-11-17 14:17:07 -0500

    Appease flake8

--- accounts/tests/test_dashboard.py
+++ accounts/tests/test_dashboard.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.test import Client
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse_lazy
 
@@ -8,22 +7,23 @@ from wagtail.wagtailcore.models import Site
 from directory.models import SCAN_URL
 from directory.tests.factories import DirectoryPageFactory
 
-class ScanViewTest(TestCase):
-  @classmethod
-  def setUpTestData(cls):
-    cls.directory = DirectoryPageFactory(
-        parent=Site.objects.get().root_page,
-        scanner_form_text='Scanner form',
-        org_details_form_text='Org form text'
-    )
 
-  def test_create_url(self):
-    User = get_user_model()
-    user = User.objects.create_user(username='username', email='email@email.com', is_active=True)
-    self.client.force_login(user)
-    response = self.client.get(reverse_lazy('dashboard'))
-    expected_create_url = '{}{}'.format(self.directory.url, SCAN_URL)
-    self.assertEqual(
-      response.context['create_link'],
-      expected_create_url
-    )
+class ScanViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.directory = DirectoryPageFactory(
+            parent=Site.objects.get().root_page,
+            scanner_form_text='Scanner form',
+            org_details_form_text='Org form text'
+        )
+
+    def test_create_url(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='username', email='email@email.com', is_active=True)
+        self.client.force_login(user)
+        response = self.client.get(reverse_lazy('dashboard'))
+        expected_create_url = '{}{}'.format(self.directory.url, SCAN_URL)
+        self.assertEqual(
+            response.context['create_link'],
+            expected_create_url
+        )
