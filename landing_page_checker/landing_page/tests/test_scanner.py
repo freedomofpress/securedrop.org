a1d5155dced32406ef5a21e2c20defdbe7144019
Harris Lapiroff <harris@chromamine.com>
2018-03-16 17:08:27 -0400

    Add tests for new scan functionslity
    Also, fix VCRs for live tests

--- landing_page_checker/landing_page/tests/test_scanner.py
+++ landing_page_checker/landing_page/tests/test_scanner.py
@@ -10,6 +10,7 @@ from landing_page_checker.landing_page.tests.utils import (
     requests_get_mock,
 )
 from landing_page_checker.models import SecuredropPage
+from landing_page_checker.tests.factories import SecuredropPageFactory
 
 
 VCR_DIR = os.path.join(os.path.dirname(__file__), 'scans_vcr')
@@ -55,3 +56,92 @@ class ScannerTest(TestCase):
         url = 'securedrop.org'
         page, soup = scanner.request_and_scrape_page(url)
         self.assertIn('SecureDrop Directory', str(page.content))
+
+    @vcr.use_cassette(os.path.join(VCR_DIR, 'full-scan-site-live.yaml'))
+    def test_scan_and_commit(self):
+        securedrop = SecuredropPageFactory.create(
+            title='Freedom of the Press Foundation',
+            landing_page_domain='https://securedrop.org',
+            onion_address='notreal.onion'
+        )
+        self.assertEqual(
+            0, SecuredropPage.objects.get(pk=securedrop.pk).results.count()
+        )
+        scanner.scan(securedrop, commit=True)
+        self.assertEqual(
+            1, SecuredropPage.objects.get(pk=securedrop.pk).results.count()
+        )
+
+    @vcr.use_cassette(os.path.join(VCR_DIR, 'full-scan-site-live.yaml'))
+    def test_scan_and_no_commit(self):
+        securedrop = SecuredropPageFactory.create(
+            title='Freedom of the Press Foundation',
+            landing_page_domain='https://securedrop.org',
+            onion_address='notreal.onion'
+        )
+        scanner.scan(securedrop)
+        self.assertEqual(
+            0, SecuredropPage.objects.get(pk=securedrop.pk).results.count()
+        )
+
+    @vcr.use_cassette(os.path.join(VCR_DIR, 'bulk-scan.yaml'))
+    def test_bulk_scan(self):
+        SecuredropPageFactory.create(
+            title='SecureDrop',
+            landing_page_domain='https://securedrop.org',
+            onion_address='notreal.onion'
+        )
+        SecuredropPageFactory.create(
+            title='Freedom of the Press Foundation',
+            landing_page_domain='https://freedom.press',
+            onion_address='notreal-2.onion'
+        )
+
+        securedrop_pages_qs = SecuredropPage.objects.all()
+        scanner.bulk_scan(securedrop_pages_qs)
+
+        for page in SecuredropPage.objects.all():
+            self.assertEqual(
+                1, page.results.count()
+            )
+
+    @mock.patch(
+        'landing_page_checker.landing_page.scanner.requests.get',
+        new=requests_get_mock
+    )
+    @mock.patch(
+        'pshtt.pshtt.requests.get',
+        new=requests_get_mock
+    )
+    @vcr.use_cassette(os.path.join(VCR_DIR, 'bulk-scan-not-live.yaml'))
+    def test_bulk_scan_not_live(self):
+        "Scans should save reasonable results even when one site is not live"
+
+        sd1 = SecuredropPageFactory.create(
+            title='SecureDrop',
+            landing_page_domain='https://securedrop.org',
+            onion_address='notreal.onion'
+        )
+        sd2 = SecuredropPageFactory.create(
+            title='Freedom of the Press Foundation',
+            landing_page_domain=NON_EXISTENT_URL,
+            onion_address='notreal-2.onion'
+        )
+        sd3 = SecuredropPageFactory.create(
+            title='Freedom of the Press Foundation',
+            landing_page_domain='https://freedom.press',
+            onion_address='notreal-3.onion'
+        )
+
+        securedrop_pages_qs = SecuredropPage.objects.all()
+        scanner.bulk_scan(securedrop_pages_qs)
+
+        self.assertTrue(
+            SecuredropPage.objects.get(pk=sd1.pk).results.all()[0].live
+        )
+        self.assertFalse(
+            SecuredropPage.objects.get(pk=sd2.pk).results.all()[0].live
+        )
+        self.assertTrue(
+            SecuredropPage.objects.get(pk=sd3.pk).results.all()[0].live
+        )
