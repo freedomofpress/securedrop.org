0f69798a1faabd2b021ff7da44730f01fb15347a
Harris Lapiroff <harris@chromamine.com>
2018-04-11 11:43:46 -0400

    Make Release objects editable

--- github/models.py
+++ github/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailsnippets.models import register_snippet
 
+
+@register_snippet
 class Release(models.Model):
     url = models.URLField(blank=False, null=False)
     tag_name = models.CharField(
@@ -9,6 +13,12 @@ class Release(models.Model):
         null=False,
     )
     date = models.DateTimeField(blank=False, null=False)
+
+    panels = [
+        FieldPanel('url'),
+        FieldPanel('tag_name'),
+        FieldPanel('date'),
+    ]
 
     def __str__(self):
         return '{} released at {} ({})'.format(
