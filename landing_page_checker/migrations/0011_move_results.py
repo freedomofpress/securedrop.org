d5ab401a894a4fd271aabee0c3e12bf7981a3cf9
Harris Lapiroff <harris@chromamine.com>
2018-04-13 14:20:59 -0400

    Ensure migrations update content types

--- landing_page_checker/migrations/0011_move_results.py
+++ landing_page_checker/migrations/0011_move_results.py
@@ -5,14 +5,32 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
+def update_contenttypes(apps, schema_migration):
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    ContentType.objects.filter(
+        app_label='landing_page_checker',
+        model='result'
+    ).update(app_label='directory')
+
+
+def update_contenttypes_reverse(apps, schema_migration):
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    ContentType.objects.filter(
+        app_label='directory',
+        model='result'
+    ).update(app_label='landing_page_checker')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('landing_page_checker', '0010_auto_20180410_1541'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
 
     database_operations = [
         migrations.AlterModelTable('Result', 'directory_result'),
+        migrations.RunPython(update_contenttypes, update_contenttypes_reverse),
     ]
 
     state_operations = [
--- landing_page_checker/migrations/0013_move_securedroppage.py
+++ landing_page_checker/migrations/0013_move_securedroppage.py
@@ -4,14 +4,32 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
+def update_contenttypes(apps, schema_migration):
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    ContentType.objects.filter(
+        app_label='landing_page_checker',
+        model='securedroppage'
+    ).update(app_label='directory')
+
+
+def update_contenttypes_reverse(apps, schema_migration):
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    ContentType.objects.filter(
+        app_label='directory',
+        model='securedroppage'
+    ).update(app_label='landing_page_checker')
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
         ('landing_page_checker', '0012_move_securedrop_owner'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
 
     database_operations = [
         migrations.AlterModelTable('SecuredropPage', 'directory_securedroppage'),
+        migrations.RunPython(update_contenttypes, update_contenttypes_reverse),
     ]
 
     state_operations = [
