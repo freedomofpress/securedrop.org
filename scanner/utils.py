3c85ed486e3b2843e60f1ae8b94fa8134aa1b597
Cameron Higby-Naquin <me@chigby.org>
2018-10-10 17:53:23 -0400

    Initial utility functions for extracting strings and urls from text

--- scanner/utils.py
+++ scanner/utils.py
@@ -1,3 +1,8 @@
+import re
+
+from typing import Set, List
+
+
 def url_to_domain(url: str) -> str:
     # Split off the protocol
     if len(url.split('//')) > 1:
@@ -5,3 +10,13 @@ def url_to_domain(url: str) -> str:
     # Split off any subpath
     url = url.split('/')[0]
     return url
+
+
+def extract_strings(text: str) -> Set[str]:
+    """Find unique quoted strings in a block of code"""
+    return set(re.findall(r'["\'](.*?)["\']', text))
+
+
+def extract_urls(text: str) -> List[str]:
+    WEB_URL_REGEX = r"""\b((?:https?:\/\/)?(?:[\da-z\.-]+)\.(?:[a-z\.]{2,6})(?:[\/\w\.-?]*)*\/?)"""
+    return re.findall(WEB_URL_REGEX, text)
