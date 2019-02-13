fc181dea83685837f5acf638ba9dd6f93b54f245
Cameron Higby-Naquin <me@chigby.org>
2019-02-13 13:00:38 -0500

    Update wagtail-factories to the latest version
    This also involved changing some code, specifically it doesn't seem
    like this wagtail factory library likes our defining parent pages as
    part of the factory definition.  This doesn't seem that bad, as I
    removed those lines and the tests worked as they should.

--- marketing/tests/factories.py
+++ marketing/tests/factories.py
@@ -7,5 +7,4 @@ from marketing.models import MarketingIndexPage
 class MarketingPageFactory(wagtail_factories.PageFactory):
     class Meta:
         model = MarketingIndexPage
-    parent = factory.SubFactory(wagtail_factories.PageFactory, parent=None)
     title = factory.Faker('sentence')
