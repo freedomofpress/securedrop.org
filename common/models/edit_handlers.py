f6fade7971db2980261a90a9ba2a6f5529fc50a9
Cameron Higby-Naquin <me@chigby.org>
2019-02-13 11:33:03 -0500

    Update `ReadOnlyPanel` for wagtail 2.0
    The internal EditHandler api has changed slightly, so we need to fix
    our use of it.

--- common/models/edit_handlers.py
+++ common/models/edit_handlers.py
@@ -2,11 +2,24 @@ import datetime
 from django.forms.utils import pretty_name
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
 from wagtail.admin.edit_handlers import EditHandler
 
 
-class BaseReadOnlyPanel(EditHandler):
+class ReadOnlyPanel(EditHandler):
+    def __init__(self, attr, heading=None, classname='', help_text=''):
+        self.attr = attr
+        self.heading = pretty_name(self.attr) if heading is None else heading
+        self.classname = classname
+        self.help_text = help_text
+
+    def clone(self):
+        return self.__class__(
+            attr=self.attr,
+            heading=self.heading,
+            classname=self.classname,
+            help_text=self.help_text,
+        )
+
     def get_value(self):
         value = getattr(self.instance, self.attr)
         if isinstance(value, datetime.date):
@@ -18,25 +31,13 @@ class BaseReadOnlyPanel(EditHandler):
     def render_as_object(self):
         template = "common/edit_handlers/read_only_object.html"
         return mark_safe(render_to_string(template, {  # nosec
-            'label': self.label,
+            'heading': self.heading,
             'value': self.get_value(),
         }))
 
     def render_as_field(self):
         template = "common/edit_handlers/read_only_field.html"
         return mark_safe(render_to_string(template, {  # nosec
-            'label': self.label,
+            'heading': self.heading,
             'value': self.get_value(),
         }))
-
-
-class ReadOnlyPanel:
-    def __init__(self, attr, label=None, classname=''):
-        self.attr = attr
-        self.label = pretty_name(self.attr) if label is None else label
-        self.classname = classname
-
-    def bind_to_model(self, model):
-        return type(str(_('ReadOnlyPanel')), (BaseReadOnlyPanel,),
-                    {'attr': self.attr, 'label': self.label,
-                     'classname': self.classname})
