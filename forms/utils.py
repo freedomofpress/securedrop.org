004e4ce62f1c4e3f784c6525d77a664dca745be5
Harris Lapiroff <harris@chromamine.com>
2019-07-09 14:43:54 -0400

    Eliminate 'Auto-Submitted: auto-generated' email header
    Wagtail 2.3 introduced this header to emails generated by
    AbstractEmailForm pages. Unfortunately, some of our email forms
    feed directly into Redmine, which discards incoming emails with
    that header (to avoid creating tickets from autoresponders, etc.)
    
    This commit patches our FormPage subclass's send_mail method to
    use our own send_mail function that omits the header. It consists
    of the following changes:
    
    1. Install wagtail-foliage for easier creation of test site trees
    2. Create a test that emails generated by a FormPage don't include
       the header
    3. Create a send_mail clone function that does not include the
       header
    4. Override the FormPage.send_mail method to call our send_mail
       clone

---
+++ forms/utils.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.core.mail.message import EmailMultiAlternatives
+from django.core.mail import get_connection
+
+
+def send_mail(subject, message, recipient_list, from_email=None, **kwargs):
+    """
+    Modified clone of wagtail.admin.utils,send_mail that omits the
+    'Auto-Submitted: auto-generated' header
+
+    This is motivated by the fact that Redmine discards messages containing
+    that header and we use FormPage emails to auto-create Redmine tickets
+
+    See: https://github.com/freedomofpress/infrastructure/issues/1982
+    """
+
+    if not from_email:
+        if hasattr(settings, 'WAGTAILADMIN_NOTIFICATION_FROM_EMAIL'):
+            from_email = settings.WAGTAILADMIN_NOTIFICATION_FROM_EMAIL
+        elif hasattr(settings, 'DEFAULT_FROM_EMAIL'):
+            from_email = settings.DEFAULT_FROM_EMAIL
+        else:
+            from_email = 'webmaster@localhost'
+
+    connection = kwargs.get('connection', False) or get_connection(
+        username=kwargs.get('auth_user', None),
+        password=kwargs.get('auth_password', None),
+        fail_silently=kwargs.get('fail_silently', None),
+    )
+    multi_alt_kwargs = {'connection': connection}
+    mail = EmailMultiAlternatives(subject, message, from_email, recipient_list, **multi_alt_kwargs)
+    html_message = kwargs.get('html_message', None)
+    if html_message:
+        mail.attach_alternative(html_message, 'text/html')
+
+    return mail.send()
