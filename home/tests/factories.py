b1f16314c16187c216bd92c9cc6a0ee73aa8f9b0
Cameron Higby-Naquin <me@chigby.org>
2019-08-07 15:11:00 -0400

    factories: Add factory for linking-object HomePageInstances

--- home/tests/factories.py
+++ home/tests/factories.py
@@ -1,15 +1,29 @@
-import factory
-import wagtail_factories
+from factory import (
+    Faker,
+    DjangoModelFactory,
+    Sequence,
+    SubFactory,
+)
+from wagtail_factories import PageFactory
 
-from home.models import HomePage
+from home.models import HomePage, HomePageInstances
 
 
-class HomePageFactory(wagtail_factories.PageFactory):
+class HomePageFactory(PageFactory):
     class Meta:
         model = HomePage
 
     title = 'SecureDrop'
     slug = 'home'
-    description_header = factory.Faker('sentence', nb_words=4)
-    description = factory.Faker('text')
+    description_header = Faker('sentence', nb_words=4)
+    description = Faker('text')
     features_header = 'What SecureDrop Does'
+
+
+class HomePageInstancesFactory(DjangoModelFactory):
+    class Meta:
+        model = HomePageInstances
+
+    sort_order = Sequence(int)
+    page = SubFactory(HomePageFactory)
+    instance = None
