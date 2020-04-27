23abec515e1075e8438c80e3967cd28f5ee7bcc4
Cameron Higby-Naquin <me@chigby.org>
2020-04-27 16:25:21 -0400

    Use PNG instead of SVG for fallback icon image format
    SVG not supported by Tor Browser, which is the case we're specifically
    targeting with these fallback options.  So use a supported image
    format instead.
    
    Also I fixed the button layout in case the document name wraps onto
    multiple lines.

--- client/common/js/embed-pdf.js
+++ client/common/js/embed-pdf.js
@@ -3,9 +3,7 @@ import PDFObject from 'pdfobject';
 function embedPdf(el) {
 	var options = {
 		fallbackLink: `<a class="pdfobject-fallback" href="[url]">
-<svg class="pdfobject-fallback__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
-    <path d="M0 0v8h7v-4h-4v-4h-3zm4 0v3h3l-3-3z" fill="white"></path>
-</svg>
+        <img class="pdfobject-fallback__icon" src="${el.dataset.iconUri}" alt="Document Icon">
 		<span>Download <strong>${el.dataset.fileTitle}</strong></span></a>`,
 	}
 	PDFObject.embed(el.dataset.fileUri, el, options)
