60a91f178e284ab403c2375b32618edf7f598ada
Maeve Andrews <maeve@freedom.press>
2020-08-05 17:01:45 -0400

    Correct default base URL

--- securedrop/settings/production.py
+++ securedrop/settings/production.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 # Domain specific
 #
-BASE_URL = os.environ.get('DJANGO_BASE_URL', 'https://freedom.press')
+BASE_URL = os.environ.get('DJANGO_BASE_URL', 'https://securedrop.org')
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', '/static/')
 MEDIA_URL = os.environ.get('DJANGO_MEDIA_URL', '/media/')
 
