d8ba3e1a2ff60ab6b9a8c162435b07cbc137145d
Cameron Higby-Naquin <me@chigby.org>
2021-11-24 15:35:26 -0500

    Create hstore extension via migrations
    Previously we were doing this via a script that worked slightly
    differently on CI and for local development, and this simplifies that
    entire situation.

--- devops/docker/postgres-scripts/hstore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Enable hstore extension on databases
-for databasename in template1 ${POSTGRES_DB}; do
-    psql -U "${POSTGRES_USER}" -c "CREATE EXTENSION IF NOT EXISTS hstore" "${databasename}"
-done
