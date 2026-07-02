7ef5867d1a0fd87df90d75aa085da272c61f95af
Paul Schreiber <paul@freedom.press>
2026-07-02 16:27:43 -0400

    fix: move WHITENOISE_ROOT to securedrop/static

--- securedrop/settings/base.py
+++ securedrop/settings/base.py
@@ -106,7 +106,7 @@ if os.environ.get("DJANGO_WHITENOISE"):
     STORAGES["staticfiles"] = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     }
-    WHITENOISE_ROOT = os.path.join(BASE_DIR, "securedrop", "static", "favicon")
+    WHITENOISE_ROOT = os.path.join(BASE_DIR, "securedrop", "static")
 
 MIDDLEWARE.extend(
     [
