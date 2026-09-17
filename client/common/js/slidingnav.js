2893218986c492e1383a35d1cbfc136595eda1ed
Paul Schreiber <paul@freedom.press>
2026-09-17 14:25:25 -0400

    style: reformat JS with eslint and prettier

--- client/common/js/slidingnav.js
+++ client/common/js/slidingnav.js
@@ -1,49 +1,51 @@
 class SlidingNav {
 	constructor(navEl, triggerEl, overlayEl) {
-		this.active = false
-		this.navEl = navEl
-		this.triggerEl = triggerEl
-		this.overlayEl = overlayEl
-		this.toggleSlide = this.toggleSlide.bind(this)
-		this.overlayEl.addEventListener('click', this.toggleSlide)
-		this.triggerEl.addEventListener('click', this.toggleSlide)
+		this.active = false;
+		this.navEl = navEl;
+		this.triggerEl = triggerEl;
+		this.overlayEl = overlayEl;
+		this.toggleSlide = this.toggleSlide.bind(this);
+		this.overlayEl.addEventListener("click", this.toggleSlide);
+		this.triggerEl.addEventListener("click", this.toggleSlide);
 	}
 
 	toggleSlide(event) {
-		event.preventDefault()
-		this.active = !this.active
-		this.render()
+		event.preventDefault();
+		this.active = !this.active;
+		this.render();
 	}
 
 	render() {
-		const { navEl, triggerEl, overlayEl } = this
+		const { navEl, triggerEl, overlayEl } = this;
 		if (this.active) {
-			document.body.classList.add('no-scroll')
-			navEl.classList.add('sliding-nav--is-active')
-			triggerEl.classList.add('is-active')
-			overlayEl.classList.add('shade--is-active')
-			window.addEventListener('resize', this.toggleSlide)
+			document.body.classList.add("no-scroll");
+			navEl.classList.add("sliding-nav--is-active");
+			triggerEl.classList.add("is-active");
+			overlayEl.classList.add("shade--is-active");
+			window.addEventListener("resize", this.toggleSlide);
 		} else {
-			document.body.classList.remove('no-scroll')
-			navEl.classList.remove('sliding-nav--is-active')
-			triggerEl.classList.remove('is-active')
-			overlayEl.classList.remove('shade--is-active')
-			window.removeEventListener('resize', this.toggleSlide)
+			document.body.classList.remove("no-scroll");
+			navEl.classList.remove("sliding-nav--is-active");
+			triggerEl.classList.remove("is-active");
+			overlayEl.classList.remove("shade--is-active");
+			window.removeEventListener("resize", this.toggleSlide);
 		}
 	}
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	 // If js is running, adjust nav menus
-	const navMenu = document.getElementById('js-nav-menu')
-	const navButton = document.getElementById('js-nav-button')
-	navMenu.classList.add('js-nav-menu-hidden')
-	navMenu.setAttribute('aria-hidden', "true")
-	navButton.classList.remove('js-nav-menu-hidden')
-	navButton.setAttribute('aria-hidden', "false")
+document.addEventListener("DOMContentLoaded", () => {
+	// If js is running, adjust nav menus
+	const navMenu = document.getElementById("js-nav-menu");
+	const navButton = document.getElementById("js-nav-button");
+	navMenu.classList.add("js-nav-menu-hidden");
+	navMenu.setAttribute("aria-hidden", "true");
+	navButton.classList.remove("js-nav-menu-hidden");
+	navButton.setAttribute("aria-hidden", "false");
 	// We're assuming there's only one sliding nav to be instantiated
-	const navElement = document.getElementsByClassName('js-sliding-nav')[0]
-	const triggerElement = navButton
-	const overlayElement = document.getElementsByClassName('js-sliding-nav-overlay')[0]
-	const slidingNav = new SlidingNav(navElement, triggerElement, overlayElement)
-})
+	const navElement = document.getElementsByClassName("js-sliding-nav")[0];
+	const triggerElement = navButton;
+	const overlayElement = document.getElementsByClassName(
+		"js-sliding-nav-overlay",
+	)[0];
+	new SlidingNav(navElement, triggerElement, overlayElement);
+});
--- client/tor/js/detect-tor.js
+++ client/tor/js/detect-tor.js
@@ -1,65 +1,67 @@
 // This user agent string matches Tor Browser 9 and 10 or Firefox Quantum (on desktop)
-const TBB_UA_REGEX = /Mozilla\/5\.0 \((Windows NT 10\.0|X11; Linux x86_64|Macintosh; Intel Mac OS X 10\.[0-9]{2}|Windows NT 10\.0; Win64; x64|Android( [0-9]{2})?; Mobile); rv:[0-9]{2,3}\.0\) Gecko\/20100101 Firefox\/([0-9]{2,3})\.0/
+const TBB_UA_REGEX =
+	/Mozilla\/5\.0 \((Windows NT 10\.0|X11; Linux x86_64|Macintosh; Intel Mac OS X 10\.[0-9]{2}|Windows NT 10\.0; Win64; x64|Android( [0-9]{2})?; Mobile); rv:[0-9]{2,3}\.0\) Gecko\/20100101 Firefox\/([0-9]{2,3})\.0/;
 
 // Use Tor css resource loading to check whether it's Tor Browser
-const is_tor_resource_loaded = async () => new Promise(resolve => {
-	setTimeout(() => resolve(false), 150)
-	try {
-		let css = document.createElement("link")
-		css.href = "resource://torbutton-assets/aboutTor.css"
-		css.type = "text/css"
-		css.rel = "stylesheet"
-		document.head.appendChild(css)
-		css.onload = function() {
-			resolve(true)
+const is_tor_resource_loaded = async () =>
+	new Promise((resolve) => {
+		setTimeout(() => resolve(false), 150);
+		try {
+			let css = document.createElement("link");
+			css.href = "resource://torbutton-assets/aboutTor.css";
+			css.type = "text/css";
+			css.rel = "stylesheet";
+			document.head.appendChild(css);
+			css.onload = function () {
+				resolve(true);
+			};
+			css.onerror = function () {
+				resolve(false);
+			};
+			document.head.removeChild(css);
+		} catch {
+			resolve(false);
 		}
-		css.onerror = function() {
-			resolve(false)
-		}
-		document.head.removeChild(css)
-	} catch(e) {
-		resolve(false)
-	}
-})
+	});
 
 const is_likely_mobile_browser = function () {
-	return window.navigator.userAgent.indexOf("Mobi") !== -1
-}
+	return window.navigator.userAgent.indexOf("Mobi") !== -1;
+};
 
 const is_likely_tor_browser = async function () {
 	return (
 		// Tor Browser has the Tor/FF UA string
-		window.navigator.userAgent.match(TBB_UA_REGEX) &&
-		// Tor Browser always reports a GMT timezone
-		new Date().getTimezoneOffset() == 0 &&
-		// Tor Browser always reports device dimensions being the same
-		// as window dimensions -- this is only true in a browser that
-		// implements letterboxing, such as Firefox configured with
-		// privacy.resistFingerprinting=true
-		window.screen.width == window.innerWidth &&
-		window.screen.height == window.innerHeight
-	) || await is_tor_resource_loaded()
-}
+		(window.navigator.userAgent.match(TBB_UA_REGEX) &&
+			// Tor Browser always reports a GMT timezone
+			new Date().getTimezoneOffset() == 0 &&
+			// Tor Browser always reports device dimensions being the same
+			// as window dimensions -- this is only true in a browser that
+			// implements letterboxing, such as Firefox configured with
+			// privacy.resistFingerprinting=true
+			window.screen.width == window.innerWidth &&
+			window.screen.height == window.innerHeight) ||
+		(await is_tor_resource_loaded())
+	);
+};
 
 const is_likely_desktop_tor_browser = async function () {
-	return !is_likely_mobile_browser() && await is_likely_tor_browser()
-}
-
+	return !is_likely_mobile_browser() && (await is_likely_tor_browser());
+};
 
 // Adjust <html> element classes according to tor detection
-is_likely_tor_browser().then(result => {
-	document.documentElement.classList.add(result ? 'tor' : 'no-tor')
-})
+is_likely_tor_browser().then((result) => {
+	document.documentElement.classList.add(result ? "tor" : "no-tor");
+});
 
 // Warn about using Javascript and not using Tor Browser
 document.addEventListener("DOMContentLoaded", async () => {
-	if (sessionStorage.getItem('torWarningDismissed') === '1') {
-		return
+	if (sessionStorage.getItem("torWarningDismissed") === "1") {
+		return;
 	}
 
-	const useTorBrowser = document.getElementById('js-use-tor-browser')
-	const instances = document.getElementById('js-instances')
-	const body = document.body
+	const useTorBrowser = document.getElementById("js-use-tor-browser");
+	const instances = document.getElementById("js-instances");
+	const body = document.body;
 
 	if (await is_likely_desktop_tor_browser()) {
 		/* If the source is using Tor Browser in desktop, we want to encourage them to turn Tor
@@ -67,77 +69,81 @@ document.addEventListener("DOMContentLoaded", async () => {
 			methods, including disabling Javascript. Since JS is disabled by turning
 			the Security Slider to "High", this code only runs if it set to another
 			(less hardened) setting. */
-		let torWarning = document.getElementById('js-tor-warning')
-		torWarning.classList.remove('tor-warning--hidden')
-		torWarning.setAttribute('aria-hidden', 'false')
+		let torWarning = document.getElementById("js-tor-warning");
+		torWarning.classList.remove("tor-warning--hidden");
+		torWarning.setAttribute("aria-hidden", "false");
 		//  hides the warning to use tor, since users already have it
-		useTorBrowser.classList.add('tor-warning--hidden')
-		useTorBrowser.setAttribute('aria-hidden', 'true')
+		useTorBrowser.classList.add("tor-warning--hidden");
+		useTorBrowser.setAttribute("aria-hidden", "true");
 		// adds class to body that disables scrolling
-		body.classList.add('no-scroll')
+		body.classList.add("no-scroll");
 		// Tell instances and updates that there's a warning
 		// so that homepage styles are adjusted
-		if(instances) {
-			instances.classList.add('instances--tor-warning')
+		if (instances) {
+			instances.classList.add("instances--tor-warning");
 		}
 
-		const closeUseTorBrowser = document.getElementById('js-tor-warning-close')
+		const closeUseTorBrowser = document.getElementById("js-tor-warning-close");
 
-		closeUseTorBrowser.addEventListener('click', () => {
-			torWarning.classList.add('tor-warning--hidden')
+		closeUseTorBrowser.addEventListener("click", () => {
+			torWarning.classList.add("tor-warning--hidden");
 			// hides warning for screen readers
-			torWarning.setAttribute('aria-hidden', 'true')
-			body.classList.remove('no-scroll')
-			if(instances) {
-				instances.classList.remove('instances--tor-warning')
+			torWarning.setAttribute("aria-hidden", "true");
+			body.classList.remove("no-scroll");
+			if (instances) {
+				instances.classList.remove("instances--tor-warning");
 			}
-			sessionStorage.setItem('torWarningDismissed', '1')
-		})
+			sessionStorage.setItem("torWarningDismissed", "1");
+		});
 	} else if (is_likely_mobile_browser()) {
-		let torWarning = document.getElementById('js-tor-mobile-warning')
-		torWarning.classList.remove('tor-warning--hidden')
-		torWarning.setAttribute('aria-hidden', 'false')
+		let torWarning = document.getElementById("js-tor-mobile-warning");
+		torWarning.classList.remove("tor-warning--hidden");
+		torWarning.setAttribute("aria-hidden", "false");
 		//  hides the warning to use tor, since users already have it
-		useTorBrowser.classList.add('tor-warning--hidden')
-		useTorBrowser.setAttribute('aria-hidden', 'true')
+		useTorBrowser.classList.add("tor-warning--hidden");
+		useTorBrowser.setAttribute("aria-hidden", "true");
 		// adds class to body that disables scrolling
-		body.classList.add('no-scroll')
+		body.classList.add("no-scroll");
 		// Tell instances and updates that there's a warning
 		// so that homepage styles are adjusted
-		if(instances) {
-			instances.classList.add('instances--tor-warning')
+		if (instances) {
+			instances.classList.add("instances--tor-warning");
 		}
 
-		const closeUseTorBrowser = document.getElementById('js-tor-mobile-warning-close')
+		const closeUseTorBrowser = document.getElementById(
+			"js-tor-mobile-warning-close",
+		);
 
-		closeUseTorBrowser.addEventListener('click', () => {
-			torWarning.classList.add('tor-warning--hidden')
+		closeUseTorBrowser.addEventListener("click", () => {
+			torWarning.classList.add("tor-warning--hidden");
 			// hides warning for screen readers
-			torWarning.setAttribute('aria-hidden', 'true')
-			body.classList.remove('no-scroll')
-			if(instances) {
-				instances.classList.remove('instances--tor-warning')
+			torWarning.setAttribute("aria-hidden", "true");
+			body.classList.remove("no-scroll");
+			if (instances) {
+				instances.classList.remove("instances--tor-warning");
 			}
-			sessionStorage.setItem('torWarningDismissed', '1')
-		})
+			sessionStorage.setItem("torWarningDismissed", "1");
+		});
 	} else {
 		// If the user is not using Tor Browser, we want to encourage them to do so.
-		useTorBrowser.classList.remove('tor-warning--hidden')
-		useTorBrowser.setAttribute('aria-hidden', 'false')
-		body.classList.add('no-scroll')
-		if(instances) {
-			instances.classList.add('instances--tor-warning')
+		useTorBrowser.classList.remove("tor-warning--hidden");
+		useTorBrowser.setAttribute("aria-hidden", "false");
+		body.classList.add("no-scroll");
+		if (instances) {
+			instances.classList.add("instances--tor-warning");
 		}
 
-		const closeUseTorBrowser = document.getElementById('js-use-tor-browser-close')
-		closeUseTorBrowser.addEventListener('click', () => {
-			useTorBrowser.classList.add('tor-warning--hidden')
-			useTorBrowser.setAttribute('aria-hidden', 'true')
-			body.classList.remove('no-scroll')
-			if(instances) {
-				instances.classList.remove('instances--tor-warning')
+		const closeUseTorBrowser = document.getElementById(
+			"js-use-tor-browser-close",
+		);
+		closeUseTorBrowser.addEventListener("click", () => {
+			useTorBrowser.classList.add("tor-warning--hidden");
+			useTorBrowser.setAttribute("aria-hidden", "true");
+			body.classList.remove("no-scroll");
+			if (instances) {
+				instances.classList.remove("instances--tor-warning");
 			}
-			sessionStorage.setItem('torWarningDismissed', '1')
-		})
+			sessionStorage.setItem("torWarningDismissed", "1");
+		});
 	}
 });
